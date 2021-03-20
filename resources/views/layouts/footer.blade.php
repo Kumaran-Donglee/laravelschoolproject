2acb583b600a8eeecf28ed470dbdeeb66d5329f3
Kumaran Donglee <kumaranjhonnytringuyen9796@gmail.com>
2021-03-20 19:34:01 +0530

    Updated design and website

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="text-center">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+  <p>Pillaichavady, Puducherry - 605 014. | Ph: 0413-2655281-288 | FAX: 2655101 | E-Mail: info@pec.edu | Contact Us | MARRS Bye Laws | Citizen's Charter | RTI 2005 | TEQIP-III | IIS | IEDC</p> 
+</footer>
